d8ba043a78a4884c3835da0e4fda5e510d24f50c
huzaifa-codes <huzaifabusiness60@gmail.com>
2025-10-07 03:59:29 +0500

    changes updated

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.dribbble.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
